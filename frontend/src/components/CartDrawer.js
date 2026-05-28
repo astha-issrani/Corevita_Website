64de55b8cfa2bf69c36bb01dbf8fae928fd4b870
Astha Issrani <asthaissrani711@gmail.com>
2026-05-28 15:07:47 +0530

    added ft:zoom in

--- frontend/src/components/CartDrawer.js
+++ frontend/src/components/CartDrawer.js
@@ -4,7 +4,7 @@ import axios from 'axios';
 import { useCart } from '../context/CartContext';
 import './CartDrawer.css';
 
-const API = process.env.REACT_APP_API_URL || 'http://localhost:5000/api';
+const API = (process.env.REACT_APP_API_URL || 'http://localhost:5000').replace(/\/api$/, '');
 
 export default function CartDrawer() {
   const {
--- frontend/src/context/ThemeContext.js
+++ frontend/src/context/ThemeContext.js
@@ -1,7 +1,7 @@
 import React, { createContext, useContext, useState, useEffect } from 'react';
 import axios from 'axios';
 
-const API = process.env.REACT_APP_API_URL || 'http://localhost:5000/api';
+const API = (process.env.REACT_APP_API_URL || 'http://localhost:5000').replace(/\/api$/, '');
 
 // Available Google Fonts grouped by style
 export const FONT_OPTIONS = [
